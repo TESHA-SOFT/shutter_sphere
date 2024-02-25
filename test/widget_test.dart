b0c36dcb93c20657ccf925718d0f2d9910e7ca36
Akilbayeva Anna <akilbayeva.anna@mail.ru>
2024-02-25 22:52:43 +0300

    package: update libraries

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,6 @@
 // tree, read text, and verify that the values of widget properties are correct.
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:shutter_sphere/main.dart';
 import 'package:shutter_sphere/src/common/routes/routes.dart';
 
 void main() {
