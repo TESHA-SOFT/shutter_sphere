0aa5451f6543bd3699aa4615050a15a6cbc789b0
Akilbayeva Anna <akilbayeva.anna@mail.ru>
2024-02-25 23:00:54 +0300

    Add colors to the color class

--- lib/src/common/style/style_color.dart
+++ lib/src/common/style/style_color.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static const button1 = Color.fromARGB(181, 175, 175, 175);
-  static const font1 = Color(0xffE1E1E1);
+  static const button1 = Color(0xff232327);
+  static const font1 = Color.fromARGB(255, 192, 182, 182);
   static const font2 = Color(0xffFFFFFF);
   static const font3 = Color(0xff151515);
-  static const background = Color(0xff1E1E1E);
+  static const font4 = Color.fromARGB(160, 167, 158, 158);
+  static const background = Color(0xff151515);
   static const button2 = Color(0xffFFCB11);
 }
