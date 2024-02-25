a9d885e27ddcd9c773459cb8fd6342640e5aa29d
Akilbayeva Anna <akilbayeva.anna@mail.ru>
2024-02-25 22:59:52 +0300

    Add routes to the home and the log in page

--- lib/src/common/routes/routes.dart
+++ lib/src/common/routes/routes.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:shutter_sphere/src/feauter/auth/page/start_page/start_page.dart';
+import 'package:shutter_sphere/src/feauter/auth/pages/log_in/log_in_page.dart';
+import 'package:shutter_sphere/src/feauter/home/pages/home_page.dart';
+import 'package:shutter_sphere/src/feauter/start/start_page.dart';
 import '../style/style_view.dart';
 
 class MyApp extends StatelessWidget {
@@ -17,6 +19,8 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => StartPage(),
+        '/home': (context) => Home(),
+        '/login': (context) => LogIn(),
       },
     );
   }
