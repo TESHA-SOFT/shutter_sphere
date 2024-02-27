69fcdfda71335689976c1eca7d8f0809192990fa
Anna Akilbayeva <akilbayeva.anna@mail.ru>
2024-02-27 16:15:27 +0300

    refactot

--- lib/src/feauter/home/pages/home_page.dart
+++ lib/src/feauter/home/pages/home_page.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:shutter_sphere/src/common/style/style_view.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: AppColor.background,
+          leading: Padding(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+            child: IconButton(
+              onPressed: () => Navigator.pop(context, false),
+              icon: SvgPicture.asset(
+                'assets/icons/arrow.svg',
+                width: 40,
+              ),
+            ),
+          )),
+      body: Text("home"),
+    );
   }
 }
