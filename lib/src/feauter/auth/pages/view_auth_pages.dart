a5195291c05f3b647adcf03fa2a061d1bbe37802
Anna Akilbayeva <akilbayeva.anna@mail.ru>
2024-02-27 16:07:37 +0300

    Сreate view for pages and widget auth

---
+++ lib/src/feauter/auth/pages/view_auth_pages.dart
@@ -0,0 +1,5 @@
+export 'log_in/log_in_page.dart';
+export 'log_in/reset_password_page.dart';
+export 'log_in/update_password_page.dart';
+export 'sign_up/profile_page.dart';
+export 'sign_up/sign_up_page.dart';
---
+++ lib/src/feauter/auth/widget/view_auth_widget.dart
@@ -0,0 +1,4 @@
+export 'app_bar_widget.dart';
+export 'text_form_password.dart';
+export 'text_form_widget.dart';
+export 'valid_app.dart';
