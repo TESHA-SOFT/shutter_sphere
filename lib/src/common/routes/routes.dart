10bd8cddd68cf6918aa0fb3b5d4519980b942908
Anna Akilbayeva <akilbayeva.anna@mail.ru>
2024-03-01 15:49:10 +0300

    Add route to the verify email page

--- lib/src/common/routes/routes.dart
+++ lib/src/common/routes/routes.dart
@@ -1,3 +1,4 @@
+import 'package:shutter_sphere/src/feauter/auth/pages/verify_email/verify_email_page.dart';
 import '../../feauter/auth/pages/view_auth_pages.dart';
 import '../../feauter/home/pages/home_page.dart';
 import '../../feauter/start/start_page.dart';
@@ -11,5 +12,6 @@ class Routes {
     '/profilreg': (context) => ProfilePage(),
     '/updatepas': (context) => UpdatePassword(),
     '/resetpas': (context) => ResetPassword(),
+    '/verifyemail': (context) => VerifyEmail(),
   };
 }
